2f43bbac855315e130987bece3e165f03557c57a
MUHMMADSALEH <khanmohdsaleh2002@gmail.com>
2024-09-20 00:02:58 +0530

    feat:add delete and update functionality

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -3,10 +3,11 @@ import { Context } from '../contexts/Context'
 
 const User = () => {
   const {user}=useContext(Context);
+  console.log(user?.user?.name)
   return (
     <div className='flex items-center gap-2'>
       <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="User" className='w-[30px] h-[30px]' />
-      {/* <span className='font-bold'>{user.name}</span> */}
+      <span className='font-bold'>{user?.user?.name}</span>
     </div>
   )
 }
--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,55 +1,96 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useParams ,useNavigate} from "react-router-dom";
-import {useContext }from "react"
+import { useParams, useNavigate } from "react-router-dom";
+import { useContext } from "react"
 import { Context } from "../contexts/Context";
 const Blog = () => {
   const [blog, setBlog] = useState({});
-const {dispatch}=useContext(Context)
-const navigate=useNavigate()
+  const [content, setContent] = useState("")
+  const [title, setTitle] = useState("")
+  const [updateMode, setUpdateMode] = useState(false)
+  const { dispatch } = useContext(Context)
+  const navigate = useNavigate()
   const { id } = useParams()
-  console.log("id  :", id)
+  // console.log("id  :", id)
 
-  const handleDelete=async (e)=>{
-
-    e.preventDefault()
-    try {
-await axios.delete(`/api/blog/${id}`)
-      navigate("/")
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  
-  const handleUpdate=async (e)=>{
-
-    e.preventDefault()
-    try {
-const res=await axios.update(`/api/blog/${id}`)
-      
-    } catch (error) {
-      console.log(error)
-    }
-  }
 
   useEffect(() => {
     const fetchBlogs = async () => {
       const res = await axios.get(`/api/blog/${id}`);
       if (res.data?.status) {
         setBlog(res.data.blogs);
+        setContent(res.data.blogs.content)
+        setTitle(res.data.blogs.title)
       }
     }
     fetchBlogs();
   }, [id])
-  console.log("blog  :",blog)
+
+  const handleDelete = async (e) => {
+
+    e.preventDefault()
+    try {
+      await axios.delete(`/api/blog/${blog._id}`)
+      navigate("/")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handelUpdate=async(e)=>{
+    console.log("hi fron delete")
+e.preventDefault()
+try {
+  const res=await axios.put(`/api/blog/${blog._id}`,{
+    title,
+    content
+  })
+ if(res.data.status){
+  setUpdateMode(false)
+  setContent(res.data.blog.content)
+  setTitle(res.data.blog.title)
+ }
+} catch (error) {
+  console.log(error)
+}
+  }
+
+
+  console.log("blog  :", blog._id)
+  console.log("blog  :", title)
+  console.log("blog  :", content)
+  console.log(updateMode)
   return (
     <div className="px-10 flex flex-col gap-10  py-5 rounded-md">
-      
-      <div className="flex flex-col gap-5 ">
-        <h1 className="font-extrabold text-2xl">{blog?.title}</h1>
-        <p className="font-extralight">{blog?.content}</p>
-      <div className="flex gap-2"> <button onClick={handleDelete}  className="text-gray-950 py-0.5 font-semibold px-1 bg-red-500 border border-gray-500 rounded-md">Delete</button>
-        <button className="text-gray-950 py-0.5 font-semibold px-1 bg-green-700 border-gray-500 rounded-md">Update</button></div>
+
+      <div className="flex flex-col gap-5">
+        {
+          updateMode ? (<input onChange={(e) => setTitle(e.target.value)} type="textarea" className="font-extrabold text-2xl outline-none border-b" value={title} />) : (<h1 className="font-extrabold text-2xl">{title}
+            <div className="flex justify-end gap-2">
+              <i
+                className="singlePostIcon far fa-edit text-green-500"
+                onClick={() => setUpdateMode(true)}
+              ></i>
+              <i
+                className="singlePostIcon far fa-trash-alt text-red-500`"
+                onClick={handleDelete}
+              ></i>
+            </div>
+
+          </h1>)
+        }
+
+        {
+          updateMode ? (<textarea type="textarea" onChange={(e) => setContent(e.target.value)} value={content} className="font-extralight  " ></textarea>) : (<p className="font-extralight">{content}</p>)
+
+        }
+        <div className="flex gap-2">
+          {
+            updateMode && <button  onClick={handelUpdate} className="text-gray-950 py-0.5 font-semibold px-1 bg-green-700 border-gray-500 rounded-md">Update</button>
+
+          }
+
+        </div>
 
       </div>
 
